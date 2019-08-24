220f3e255dac9d4cb967dafbca025c5cd2cdb2ee
andrewcgaitskell <andrew@gaitskell.com>
2019-08-24 19:39:27 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
-
 function myFunction(p1, p2) {
   return p1 * p2;   // The function returns the product of p1 and p2
 }
